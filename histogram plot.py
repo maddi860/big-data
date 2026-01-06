689bc0cf83fd47df0f128a42f914fdd0e60572e6
maddi860 <maddisont16@outlook.com>
2026-01-06 22:03:44 +0000

    Add files via upload
    changed the code to be a subplot instead of being two histograms on the same plot

--- histogram plot.py
+++ histogram plot.py
@@ -13,20 +13,19 @@ data = pd.read_csv('sub_gal.csv')#reading the data that is needed
 data_new = data[(data['zsp'] > 0) & (data['z_best'] > 0)]
 #removing zeros and other non-physical values
 
-"""creating the histogram plot"""
+"""creating the histogram plots"""
 plt.figure()#begins the plot
-plt.hist(data_new['zsp'], bins = 30, alpha = 0.6, label = 'zsp', 
-         density = True, range = (0, 1), histtype="step", linewidth=2,)
+plt.subplot(2, 1, 1)
+plt.hist(data_new['zsp'], bins = 30, alpha = 0.6)
 #the first histogram for zsp values
-plt.hist(data_new['z_best'], alpha = 0.5, bins = 30, label = 'z_best', 
-         density = True, range = (0, 1), histtype="step", linewidth=2,)
+plt.title('zsp')
+
+
+
+plt.subplot(2, 1, 2)
+plt.hist(data_new['z_best'], alpha = 0.5, bins = 30, color = 'red')
 #the second histogram for z_best values
-plt.title('Comparison of zsp and z_best')
-"""I have used the labels Redshift and Density because I have used
-Density = True which shows the probability density making the plot more easy 
-to read. The area under each histogram is equal to one. 
-For Redshift, each bin groups the values into ranges."""
-plt.xlabel('Redshift')#creates a label for the x-axis
-plt.ylabel('Density')#creates a label for the y-axis
-plt.legend()#shows which histogram plot is for each data using the labels made
-plt.show()#shows the plot
+plt.title('z_best')
+
+
+plt.show()#shows the plots
